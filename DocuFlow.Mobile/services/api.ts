b0dcb04e0c8a013d7b1b6195f14d3faabbd73afd
tnshabazkhan <tnshabazkhan@gmail.com>
2025-02-02 12:30:00 +0000

    feat(mobile): update API base URL to 192.168.0.13

--- DocuFlow.Mobile/services/api.ts
+++ DocuFlow.Mobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const API_BASE_URL = 'http://10.88.21.81:5009/api';
+const API_BASE_URL = 'http://192.168.1.101:5009/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
